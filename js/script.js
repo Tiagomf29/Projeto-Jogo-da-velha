e9c408020d876acdbd25748898ec1bba939b134f
tiagomf29 <tiagomf29@hotmail.com>
2021-04-30 21:16:21 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 
-var variavelAtual;  
+var variavelAtual; 
+var jogoAcabou = false; 
 
 window.onload = function (){
     let botao = document.querySelector("button");
-    botao.addEventListener('click', e =>{   
-        iniciaJogo();
-        let img = document.querySelector("img");
-        img.src="img/destrava.png";
-        botao.style.backgroundColor = "yellow";
+    botao.addEventListener('click', e =>{  
+        if (!jogoAcabou){
+            iniciaJogo();
+            let img = document.querySelector("img");
+            img.src="img/destrava.png";
+            botao.style.backgroundColor = "yellow";
+            botao.innerHTML = '<img src="img/trava.png" alt="cadeado travado"/>Reiniciar Jogo';
+        }else{
+            reiniciarJogo();
+        }
     });
 
 }
@@ -31,16 +37,19 @@ function exibeMarcacao(objeto){
 
     if(document.getElementById(objeto).innerHTML ==""){
 
-        if (variavelAtual == "" || variavelAtual=="O"){
-            let valor = document.getElementById(objeto).innerHTML = "X";         
-            return variavelAtual = "X";
+        if (!jogoAcabou){
+            if (variavelAtual == "" || variavelAtual=="O"){
+                let valor = document.getElementById(objeto).innerHTML = "X";         
+                return variavelAtual = "X";
+            }else{
+                let valor = document.getElementById(objeto).innerHTML = "O";
+                return variavelAtual = "O";  
+            }   
         }else{
-            let valor = document.getElementById(objeto).innerHTML = "O";
-            return variavelAtual = "O";  
-        }   
-
+            alert("Jogo acabou!");
+            return variavelAtual = "";
+        }
     }        
-
 }
 
 function validaTermino(){
@@ -52,13 +61,13 @@ function validaTermino(){
    }
 
    let dimH1 = (aJogo[1] == aJogo[2] && aJogo[1] == aJogo[3] && aJogo[1] !="");
-   if(dimH1)propriedades(0,165,220);        
+   if(dimH1)propriedades(0,180,220);        
    
    let dimH2 = (aJogo[4] == aJogo[5] && aJogo[4] == aJogo[6] && aJogo[4] !="");
    if(dimH2)propriedades(0,0,0); 
 
    let dimH3 = (aJogo[7] == aJogo[8] && aJogo[7] == aJogo[9] && aJogo[7] !="");
-   if(dimH3)propriedades(0,540,220); 
+   if(dimH3)propriedades(0,550,220); 
 
    let dimV1 = (aJogo[1] == aJogo[4] && aJogo[1] == aJogo[7] && aJogo[1] !="");    
    if(dimV1)propriedades(90,0,27);    
@@ -70,7 +79,7 @@ function validaTermino(){
    if(dimV3)propriedades(90,0,400); 
 
    let dimD1 = (aJogo[1] == aJogo[5] && aJogo[1] == aJogo[9] && aJogo[1] !="");
-   if(dimD1)propriedades(45,0,0); 
+   if(dimD1)propriedades(45,365,0); 
 
    let dimD2 = (aJogo[3] == aJogo[5] && aJogo[3] == aJogo[7] && aJogo[3] !="");
    if(dimD2)propriedades(135,0,0);
@@ -87,5 +96,19 @@ function propriedades(rotate, top, left ){
     }
     if(left != 0){
         objeto.style.left= left+"px";
-    }    
+    }
+    jogoAcabou = true;        
+}
+
+function reiniciarJogo(){
+
+    let td = document.querySelectorAll("td");
+    let objeto = document.getElementById("risco");
+    objeto.style.display = "none"; 
+    td.forEach(vlr =>{
+        vlr.innerHTML = "";
+        document.getElementById(vlr.id).style.color = "black";
+        jogoAcabou = false;
+    });
+
 }
